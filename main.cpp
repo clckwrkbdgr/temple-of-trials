6581d4991e72e44abb696b40b749396a7165577c
Igor Chaika <kp580bm1@gmail.com>
2013-11-03 16:18:43 +0200

    It is alive\!

--- main.cpp
+++ main.cpp
@@ -28,8 +28,29 @@ int main()
 	log(now());
 	log("Start log.");
 
-	mvprintw(0, 0, "Howdy");
-	getch();
+	int x = 0, y = 0;
+	while(true) {
+		mvaddch(y, x, '@');
+
+		int ch = getch();
+		if(ch == 'q') {
+			break;
+		}
+
+		mvaddch(y, x, ' ');
+
+		switch(ch) {
+			case 'h': x--; break;
+			case 'j': y++; break;
+			case 'k': y--; break;
+			case 'l': x++; break;
+			case 'y': y--; x--; break;
+			case 'u': y--; x++; break;
+			case 'b': y++; x--; break;
+			case 'n': y++; x++; break;
+			default: break;
+		}
+	}
 
 	cbreak();
 	echo();
