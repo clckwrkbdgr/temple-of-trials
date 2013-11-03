3e0ce9554680fd0d6ecafb4034a0e83ecd6b8bee
Igor Chaika <kp580bm1@gmail.com>
2013-11-03 16:08:44 +0200

    Log routine.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include <ncurses.h>
+#include <time.h>
+
+std::string now()
+{
+	time_t t = time(0);
+	std::string s = ctime(&t);
+	return s.replace(s.find('\n'), 1, " ");
+}
+
+void log(const std::string & message)
+{
+	static std::ofstream lout("temple.log", std::ios::app);
+	lout << message << std::endl;
+}
 
 int main()
 {
@@ -9,6 +24,10 @@ int main()
 	noecho();
 	curs_set(0);
 
+	log("");
+	log(now());
+	log("Start log.");
+
 	mvprintw(0, 0, "Howdy");
 	getch();
 
@@ -16,5 +35,6 @@ int main()
 	echo();
 	curs_set(1);
 	endwin();
+	log("Exit.");
 	return 0;
 }
