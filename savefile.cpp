98e92d5c2fdf335c9d69127216fa4a79098b914f
Igor Chaika <umi0451@gmail.com>
2013-11-18 16:38:16 +0200

    Reader class for savefile with basic read functions. Version handling for savefile.

--- savefile.cpp
+++ savefile.cpp
@@ -41,93 +41,176 @@ std::string escaped(const std::string & s)
 	return '"' + result + '"';
 }
 
-#define CHECK(in, text) do { if(!(in).good()) { log("Error: savefile is corrupted (reading " + to_string(text) + ") ."); return false; } } while(0)
-bool Game::load(const std::string & filename)
+class Reader {
+public:
+	struct Exception {
+		std::string message;
+		Exception(const std::string text) : message(text) {}
+	};
+	Reader(const std::string & filename);
+
+	Reader & newline();
+	Reader & version(int version);
+	int version() const { return actual_version; }
+	void check(const std::string & section);
+	
+	Reader & store(int & value);
+	Reader & store(unsigned int & value);
+	Reader & store(size_t & value);
+	Reader & store(char & value);
+	Reader & store(bool & value);
+	Reader & store(std::string & value);
+private:
+	int actual_version;
+	std::ifstream in;
+};
+
+Reader::Reader(const std::string & filename)
+	: actual_version(SAVEFILE_VERSION), in(filename.c_str(), std::ios::in)
 {
 	if(!file_exists(filename)) {
-		return false;
+		throw Exception(format("File '{0}' doesn't exists!", filename));
 	}
-	std::ifstream in(filename.c_str(), std::ios::in);
 	if(!in) {
-		log(format("Cannot open file '{0}' for reading!", filename));
+		throw Exception(format("Cannot open file '{0}' for reading!", filename));
+	}
+}
+
+Reader & Reader::newline()
+{
+	return *this;
+}
+
+Reader & Reader::version(int version)
+{
+	in >> actual_version;
+	check("version");
+	if(actual_version > version) {
+		throw Exception(format(
+					"Savefile is of version {0}, which is incompatible with current program version {1}.",
+					actual_version, version
+					));
+	}
+	return *this;
+}
+
+void Reader::check(const std::string & section)
+{
+	if(!in.good()) {
+		throw Exception("Error: savefile is corrupted (reading " + to_string(section) + ") .");
+	}
+}
+
+Reader & Reader::store(int & value)
+{
+	in >> value;
+	return *this;
+}
+
+Reader & Reader::store(unsigned int & value)
+{
+	in >> value;
+	return *this;
+}
+
+Reader & Reader::store(size_t & value)
+{
+	in >> value;
+	return *this;
+}
+
+Reader & Reader::store(char & value)
+{
+	int stored_value;
+	in >> stored_value;
+	value = (char)stored_value;
+	return *this;
+}
+
+Reader & Reader::store(bool & value)
+{
+	int stored_value;
+	in >> stored_value;
+	value = stored_value;
+	return *this;
+}
+
+Reader & Reader::store(std::string & value)
+{
+	value = read_string(in);
+	return *this;
+}
+
+bool Game::load(const std::string & filename)
+{
+	try {
+		Reader in(filename);
+
+		in.version(SAVEFILE_VERSION);
+		unsigned width, height;
+		in.store(width).store(height);
+		in.check("map size");
+		map = Map(width, height, Cell());
+		for(unsigned y = 0; y < map.get_height(); ++y) {
+			for(unsigned x = 0; x < map.get_width(); ++x) {
+				in.store(map.cell(x, y).sprite).store(map.cell(x, y).passable);
+				in.check("map cell");
+			}
+		}
+
+		int player_sprite, player_pos_x, player_pos_y, player_ai;
+		std::string player_name;
+		if(in.version() <= 3) {
+			in.store(player_pos_x).store(player_pos_y).store(player_sprite);
+		} else if(in.version() <= 4) {
+			in.store(player_pos_x).store(player_pos_y).store(player_sprite).store(player_ai);
+		} else if(in.version() <= 5) {
+			in.store(player_pos_x).store(player_pos_y).store(player_sprite).store(player_ai).store(player_name);
+		}
+		in.check("player");
+
+		unsigned monsters_count;
+		in.store(monsters_count);
+		in.check("monster count");
+		monsters.resize(monsters_count);
+		for(unsigned i = 0; i < monsters_count; ++i) {
+			if(in.version() <= 3) {
+				in.store(monsters[i].pos.x).store(monsters[i].pos.y).store(monsters[i].sprite);
+			} else if(in.version() <= 4) {
+				int ai;
+				in.store(monsters[i].pos.x).store(monsters[i].pos.y).store(monsters[i].sprite).store(ai);
+				monsters[i].ai = AI(ai == 0 ? AI::PLAYER : AI::MONSTER, ai == 1 ? AI::STILL : AI::WANDER);
+			} else if(in.version() <= 6) {
+				int ai;
+				in.store(monsters[i].pos.x).store(monsters[i].pos.y).store(monsters[i].sprite).store(ai);
+				monsters[i].ai = AI(ai == 0 ? AI::PLAYER : AI::MONSTER, ai == 1 ? AI::STILL : AI::WANDER);
+			} else {
+				in.store(monsters[i].pos.x).store(monsters[i].pos.y).store(monsters[i].sprite);
+				in.store(monsters[i].ai.faction).store(monsters[i].ai.movement);
+				in.store(monsters[i].name);
+			}
+			in.check("monster");
+		}
+
+		unsigned doors_count;
+		in.store(doors_count);
+		in.check("door count");
+		doors.resize(doors_count);
+		for(unsigned i = 0; i < doors_count; ++i) {
+			if(in.version() <= 7) {
+				in.store(doors[i].pos.x).store(doors[i].pos.y).store(doors[i].opened);
+			} else {
+				in.store(doors[i].pos.x).store(doors[i].pos.y).store(doors[i].opened_sprite).store(doors[i].closed_sprite);
+				in.store(doors[i].opened);
+			}
+			in.check("door");
+		}
+	} catch(const Reader::Exception & e) {
+		log(e.message);
 		return false;
-	}
-
-	int version;
-	in >> version;
-	CHECK(in, "version");
-	if(version > SAVEFILE_VERSION) {
-		log(format("Savefile is of version {0}, which is incompatible with current program version {1}.", version, int(SAVEFILE_VERSION)));
-		return false;
-	}
-	unsigned width, height;
-	in >> width >> height;
-	CHECK(in, "map size");
-	map = Map(width, height, Cell());
-	for(unsigned y = 0; y < map.get_height(); ++y) {
-		for(unsigned x = 0; x < map.get_width(); ++x) {
-			int sprite, passable;
-			in >> sprite >> passable;
-			map.cell(x, y).sprite = sprite;
-			map.cell(x, y).passable = passable;
-			CHECK(in, "map cell");
-		}
-	}
-
-	int player_sprite, player_pos_x, player_pos_y, player_ai;
-	if(version <= 3) {
-		in >> player_pos_x >> player_pos_y >> player_sprite;
-	} else if(version <= 4) {
-		in >> player_pos_x >> player_pos_y >> player_sprite >> player_ai;
-	} else if(version <= 5) {
-		in >> player_pos_x >> player_pos_y >> player_sprite >> player_ai;
-		read_string(in);
-	}
-	CHECK(in, "player");
-
-	unsigned monsters_count;
-	in >> monsters_count;
-	CHECK(in, "monster count");
-	monsters.resize(monsters_count);
-	for(unsigned i = 0; i < monsters_count; ++i) {
-		int monster_sprite;
-		if(version <= 3) {
-			in >> monsters[i].pos.x >> monsters[i].pos.y >> monster_sprite;
-		} else if(version <= 4) {
-			int ai;
-			in >> monsters[i].pos.x >> monsters[i].pos.y >> monster_sprite >> ai;
-			monsters[i].ai = AI(ai == 0 ? AI::PLAYER : AI::MONSTER, ai == 1 ? AI::STILL : AI::WANDER);
-		} else if(version <= 6) {
-			int ai;
-			in >> monsters[i].pos.x >> monsters[i].pos.y >> monster_sprite >> ai;
-			monsters[i].ai = AI(ai == 0 ? AI::PLAYER : AI::MONSTER, ai == 1 ? AI::STILL : AI::WANDER);
-		} else {
-			in >> monsters[i].pos.x >> monsters[i].pos.y >> monster_sprite;
-			in >> monsters[i].ai.faction >> monsters[i].ai.movement;
-			monsters[i].name = read_string(in);
-		}
-		CHECK(in, "monster");
-		monsters[i].sprite = monster_sprite;
-	}
-
-	unsigned doors_count;
-	in >> doors_count;
-	CHECK(in, "door count");
-	doors.resize(doors_count);
-	for(unsigned i = 0; i < doors_count; ++i) {
-		if(version <= 7) {
-			in >> doors[i].pos.x >> doors[i].pos.y >> doors[i].opened;
-		} else {
-			int opened_sprite, closed_sprite;
-			in >> doors[i].pos.x >> doors[i].pos.y >> opened_sprite >> closed_sprite >> doors[i].opened;
-			doors[i].opened_sprite = opened_sprite;
-			doors[i].closed_sprite = closed_sprite;
-		}
-		CHECK(in, "door");
 	}
 	return true;
 }
-#undef CHECK
 
 class Writer {
 public:
@@ -138,6 +221,10 @@ public:
 	Writer(const std::string & filename);
 
 	Writer & newline();
+	Writer & version(int version);
+	int version() const { return actual_version; }
+	void check(const std::string & section);
+	
 	Writer & store(int value);
 	Writer & store(unsigned int value);
 	Writer & store(size_t value);
@@ -145,11 +232,12 @@ public:
 	Writer & store(bool value);
 	Writer & store(const std::string & value);
 private:
+	int actual_version;
 	std::ofstream out;
 };
 
 Writer::Writer(const std::string & filename)
-	: out(filename.c_str(), std::ios::out)
+	: actual_version(SAVEFILE_VERSION), out(filename.c_str(), std::ios::out)
 {
 	if(!out) {
 		throw Exception(format("Cannot open file '{0}' for writing!", filename));
@@ -160,6 +248,20 @@ Writer & Writer::newline()
 {
 	out << '\n';
 	return *this;
+}
+
+Writer & Writer::version(int version)
+{
+	out << version << ' ';
+	actual_version = version;
+	return *this;
+}
+
+void Writer::check(const std::string & section)
+{
+	if(!out.good()) {
+		throw Exception("Error: savefile is corrupted (writing " + to_string(section) + ") .");
+	}
 }
 
 Writer & Writer::store(int value)
@@ -202,7 +304,7 @@ bool Game::save(const std::string & filename) const
 {
 	try {
 		Writer out(filename);
-		out.store(SAVEFILE_VERSION);
+		out.version(SAVEFILE_VERSION);
 		out.newline();
 		out.store(map.get_width()).store(map.get_height());
 		out.newline();
